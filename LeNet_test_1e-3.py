97f3c9662520e45b61b68595b61265a8d4f85b87
chukexin-hfut <44255040+chukexin-hfut@users.noreply.github.com>
2018-10-18 15:54:32 +0800

    Add files via upload

---
+++ LeNet_test_1e-3.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torch.autograd import Variable
+import torch.utils.data.dataloader as Data
+from Conv2D import Conv2d
+from Linear import Linear
+from mnist import MNIST
+
+
+batch_size = 128
+
+test_data = MNIST(
+    '../MNIST', train=False, transform=torchvision.transforms.ToTensor(), download=True
+)
+print(len(test_data))
+
+test_loader = Data.DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True)
+
+
+class Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv1 = Conv2d(1, 6, 5, padding=2)
+        # 32->14
+        self.conv2 = Conv2d(6, 16, 5)
+        # 14->5
+        self.conv3 = Conv2d(16, 120, 5)
+        # 5->1
+        self.dense1 = Linear(120, 84)
+        self.dense2 = Linear(84, 10)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2)
+        self.batchnorm = nn.BatchNorm2d(1)
+
+    def forward(self, input, prob, num_bits):
+        input = self.batchnorm(input)
+        conv1_out = self.pool(self.relu(self.conv1(input, prob, num_bits)))
+        conv2_out = self.pool(self.relu(self.conv2(conv1_out, prob, num_bits)))
+        conv3_out = self.relu(self.conv3(conv2_out, prob, num_bits))
+        dense_in = conv3_out.view(conv3_out.size()[0], -1)
+        dense_out = self.relu(self.dense1(dense_in, prob, num_bits))
+        out = self.dense2(dense_out, prob, num_bits)
+        return out
+
+
+loss_func = nn.CrossEntropyLoss()
+
+
+def test(model, prob, num_bits):
+    model.eval()
+    eval_loss = 0.
+    eval_acc = 0.
+    for batch_x, batch_y in test_loader:
+        batch_x, batch_y = Variable(batch_x), Variable(batch_y)
+        out = model(batch_x, prob, num_bits)
+        # out = model(batch_x)
+        loss = loss_func(out, batch_y)
+        eval_loss += loss.data.item() * (batch_x.size()[0])
+
+        pred = torch.max(out, 1)[1]
+        num_correct = (pred == batch_y).sum()
+
+        eval_acc += num_correct.data.item()
+        print(loss.data.item(), float(num_correct.data.item()) / float(batch_x.size()[0]))
+
+    print('Test Loss: {:.6f}, Acc: {:.6f}'.format(eval_loss / len(test_data), float(eval_acc) / len(test_data)))
+
+
+def run(para_path):
+    print(para_path)
+    model = Net2()
+    model.load_state_dict(torch.load(para_path))
+    prob = 0.1
+    num_bits = 7
+    test(model, prob, num_bits)
+
+
+for epoch in range(1):
+    run('../change_Lossfunction/LeNet_L2Loss_1e-3.pkl')
---
+++ container.py
@@ -0,0 +1,235 @@
+from collections import OrderedDict, Iterable
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import collections
+from itertools import repeat
+
+class Sequential(nn.Module):
+    r"""A sequential container.
+    Modules will be added to it in the order they are passed in the constructor.
+    Alternatively, an ordered dict of modules can also be passed in.
+
+    To make it easier to understand, given is a small example::
+
+        # Example of using Sequential
+        model = nn.Sequential(
+                  nn.Conv2d(1,20,5),
+                  nn.ReLU(),
+                  nn.Conv2d(20,64,5),
+                  nn.ReLU()
+                )
+
+        # Example of using Sequential with OrderedDict
+        model = nn.Sequential(OrderedDict([
+                  ('conv1', nn.Conv2d(1,20,5)),
+                  ('relu1', nn.ReLU()),
+                  ('conv2', nn.Conv2d(20,64,5)),
+                  ('relu2', nn.ReLU())
+                ]))
+    """
+
+    def __init__(self, *args):
+        super(Sequential, self).__init__()
+        if len(args) == 1 and isinstance(args[0], OrderedDict):
+            for key, module in args[0].items():
+                self.add_module(key, module)
+        else:
+            for idx, module in enumerate(args):
+                self.add_module(str(idx), module)
+
+    def __getitem__(self, idx):
+        if not (-len(self) <= idx < len(self)):
+            raise IndexError('index {} is out of range'.format(idx))
+        if idx < 0:
+            idx += len(self)
+        it = iter(self._modules.values())
+        for i in range(idx):
+            next(it)
+        return next(it)
+
+    def __len__(self):
+        return len(self._modules)
+
+    def __dir__(self):
+        keys = super(Sequential, self).__dir__()
+        keys = [key for key in keys if not key.isdigit()]
+        return keys
+
+    def forward(self, input, mode, q_w):
+        for module in self._modules.values():
+            input = module(input, mode, q_w)
+        return input
+
+
+class MaxPool2d(nn.Module):
+    r"""Applies a 2D max pooling over an input signal composed of several input
+    planes.
+
+    In the simplest case, the output value of the layer with input size :math:`(N, C, H, W)`,
+    output :math:`(N, C, H_{out}, W_{out})` and :attr:`kernel_size` :math:`(kH, kW)`
+    can be precisely described as:
+
+    .. math::
+
+        \begin{array}{ll}
+        out(N_i, C_j, h, w)  = \max_{{m}=0}^{kH-1} \max_{{n}=0}^{kW-1}
+                               input(N_i, C_j, stride[0] * h + m, stride[1] * w + n)
+        \end{array}
+
+    | If :attr:`padding` is non-zero, then the input is implicitly zero-padded on both sides
+      for :attr:`padding` number of points
+    | :attr:`dilation` controls the spacing between the kernel points. It is harder to describe,
+      but this `link`_ has a nice visualization of what :attr:`dilation` does.
+
+    The parameters :attr:`kernel_size`, :attr:`stride`, :attr:`padding`, :attr:`dilation` can either be:
+
+        - a single ``int`` -- in which case the same value is used for the height and width dimension
+        - a ``tuple`` of two ints -- in which case, the first `int` is used for the height dimension,
+          and the second `int` for the width dimension
+
+    Args:
+        kernel_size: the size of the window to take a max over
+        stride: the stride of the window. Default value is :attr:`kernel_size`
+        padding: implicit zero padding to be added on both sides
+        dilation: a parameter that controls the stride of elements in the window
+        return_indices: if ``True``, will return the max indices along with the outputs.
+                        Useful when Unpooling later
+        ceil_mode: when True, will use `ceil` instead of `floor` to compute the output shape
+
+    Shape:
+        - Input: :math:`(N, C, H_{in}, W_{in})`
+        - Output: :math:`(N, C, H_{out}, W_{out})` where
+          :math:`H_{out} = floor((H_{in}  + 2 * padding[0] - dilation[0] * (kernel\_size[0] - 1) - 1) / stride[0] + 1)`
+          :math:`W_{out} = floor((W_{in}  + 2 * padding[1] - dilation[1] * (kernel\_size[1] - 1) - 1) / stride[1] + 1)`
+
+    Examples::
+
+        >>> # pool of square window of size=3, stride=2
+        >>> m = nn.MaxPool2d(3, stride=2)
+        >>> # pool of non-square window
+        >>> m = nn.MaxPool2d((3, 2), stride=(2, 1))
+        >>> input = autograd.Variable(torch.randn(20, 16, 50, 32))
+        >>> output = m(input)
+
+    .. _link:
+        https://github.com/vdumoulin/conv_arithmetic/blob/master/README.md
+    """
+
+    def __init__(self, kernel_size, stride=None, padding=0, dilation=1,
+                 return_indices=False, ceil_mode=False):
+        super(MaxPool2d, self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride or kernel_size
+        self.padding = padding
+        self.dilation = dilation
+        self.return_indices = return_indices
+        self.ceil_mode = ceil_mode
+
+    def forward(self, input, mode, q_w):
+        return F.max_pool2d(input, self.kernel_size, self.stride,
+                            self.padding, self.dilation, self.ceil_mode,
+                            self.return_indices)
+
+    def __repr__(self):
+        kh, kw = _pair(self.kernel_size)
+        dh, dw = _pair(self.stride)
+        padh, padw = _pair(self.padding)
+        dilh, dilw = _pair(self.dilation)
+        padding_str = ', padding=(' + str(padh) + ', ' + str(padw) + ')' \
+            if padh != 0 and padw != 0 else ''
+        dilation_str = (', dilation=(' + str(dilh) + ', ' + str(dilw) + ')'
+                        if dilh != 0 and dilw != 0 else '')
+        ceil_str = ', ceil_mode=' + str(self.ceil_mode)
+        return self.__class__.__name__ + '(' \
+            + 'kernel_size=(' + str(kh) + ', ' + str(kw) + ')' \
+            + ', stride=(' + str(dh) + ', ' + str(dw) + ')' \
+            + padding_str + dilation_str + ceil_str + ')'
+
+
+class Threshold(nn.Module):
+    r"""Thresholds each element of the input Tensor
+
+    Threshold is defined as:
+
+    .. math::
+        y =
+        \begin{cases}
+        x, &\text{ if } x > \text{threshold} \\
+        \text{value}, &\text{ otherwise }
+        \end{cases}
+
+    Args:
+        threshold: The value to threshold at
+        value: The value to replace with
+        inplace: can optionally do the operation in-place. Default: ``False``
+
+    Shape:
+        - Input: :math:`(N, *)` where `*` means, any number of additional
+          dimensions
+        - Output: :math:`(N, *)`, same shape as the input
+
+    Examples::
+
+        >>> m = nn.Threshold(0.1, 20)
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+    """
+
+    def __init__(self, threshold, value, inplace=False):
+        super(Threshold, self).__init__()
+        self.threshold = threshold
+        self.value = value
+        self.inplace = inplace
+        # TODO: check in THNN (if inplace == True, then assert value <= threshold)
+
+    def forward(self, input, mode, q_w):
+        return F.threshold(input, self.threshold, self.value, self.inplace)
+
+    def extra_repr(self):
+        inplace_str = ', inplace' if self.inplace else ''
+        return 'threshold={}, value={}{}'.format(
+            self.threshold, self.value, inplace_str
+        )
+
+
+
+class ReLU(Threshold):
+    r"""Applies the rectified linear unit function element-wise
+    :math:`\text{ReLU}(x)= \max(0, x)`
+
+    .. image:: scripts/activation_images/ReLU.png
+
+    Args:
+        inplace: can optionally do the operation in-place. Default: ``False``
+
+    Shape:
+        - Input: :math:`(N, *)` where `*` means, any number of additional
+          dimensions
+        - Output: :math:`(N, *)`, same shape as the input
+
+    Examples::
+
+        >>> m = nn.ReLU()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+    """
+
+    def __init__(self, inplace=False):
+        super(ReLU, self).__init__(0, 0, inplace)
+
+    def extra_repr(self):
+        inplace_str = 'inplace' if self.inplace else ''
+        return inplace_str
+
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, collections.Iterable):
+            return x
+        return tuple(repeat(x, n))
+    return parse
+
+_single = _ntuple(1)
+_pair = _ntuple(2)
+_triple = _ntuple(3)
+_quadruple = _ntuple(4)
